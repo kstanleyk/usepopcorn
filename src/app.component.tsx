6bbb42d072e5df98153bd0388f79c9e0a1d272e3
Stanley Kiven <kstanleyk@live.com>
2024-01-04 11:11:57 +0100

    switch to vite

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -1,19 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Logo from "./components/logo.component";
 import Navbar from "./components/nav-bar.component";
 import NumResults from "./components/num-results.component";
 import Search from "./components/search.component";
 import Main from "./pages/main.component";
 import { IMovie } from "./models/movie.model";
-import { tempMovieData, tempWatchedData } from "./data";
 import Box from "./components/box.component";
 import MovieList from "./components/movie-list.component";
 import WatchedSummary from "./components/watched-summary.component";
 import WatchedMovieList from "./components/watched-movie-list";
 
+const apikey = "40b7cd83";
+
 export default function App() {
-  const [movies, setMovies] = useState<IMovie[]>(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState<IMovie[]>([]);
+  const [watched, setWatched] = useState([]);
+
+  const query = "interstellar";
+
+  useEffect(function () {
+    async function fetchMovies() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?i=tt3896198&apikey=${apikey}&s=${query}`
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+    }
+    fetchMovies();
+  }, []);
+
   return (
     <>
       <Navbar>
